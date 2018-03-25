5786911233bc60af66cb69cdc84666dbe9fd5a4a
Andrzej Palica <and.palica@gmail.com>
2018-03-25 14:38:40 +0200

    Utwórz kontroler dla widoku task 4

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,27 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+
+
 public class Controller {
+    @FXML
+    private Button prevButton;
+    @FXML
+    private Button nextButton;
+    @FXML
+    private ImageView image;
+    @FXML
+    private MenuItem openItem;
+    @FXML
+    private MenuItem saveItem;
+    @FXML
+    private MenuItem quitItem;
+
+    public void initialize() {
+
+    }
+
+
 
 }
